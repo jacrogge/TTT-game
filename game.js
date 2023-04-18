a2e9304e0ddf90ff793aacc388c62b5dc875e186
jacrogge <131113367+jacrogge@users.noreply.github.com>
2023-04-18 12:17:30 +0800

    Update game.js

--- game.js
+++ game.js
@@ -23,21 +23,32 @@ function handleClick(event) {
 }
 
 function checkForWin() {
-	const winningCombos = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6]
-	];
-	return winningCombos.some(combo => {
-		return combo.every(index => {
-			return squares[index].textContent === currentPlayer;
-		});
-	});
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < winningCombos.length; i++) {
+    const [a, b, c] = winningCombos[i];
+    if (board[a] === player && board[b] === player && board[c] === player) {
+      // Display X or O symbol
+      const symbol = player === 'X' ? '❌' : '⭕';
+      document.querySelector('.winner').innerHTML = `${symbol} wins!`;
+
+      // Add crossline across winning squares
+      const winningSquares = document.querySelectorAll(`#${a}, #${b}, #${c}`);
+      winningSquares.forEach(square => square.classList.add('winning-square'));
+
+      return true;
+    }
+  }
+  return false;
 }
 
 function checkForDraw() {
