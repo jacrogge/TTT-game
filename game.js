58d7c5fb26331193f8fd87dfc1f9af47802b6ac9
jacrogge <131113367+jacrogge@users.noreply.github.com>
2023-04-18 12:33:54 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 const squares = document.querySelectorAll('.square');
 let currentPlayer = 'X';
-const board = ['', '', '', '', '', '', '', '', ''];
 
 squares.forEach(square => {
 	square.addEventListener('click', handleClick);
@@ -11,10 +10,8 @@ function handleClick(event) {
 	if (square.textContent !== '') {
 		return;
 	}
-	const index = Array.from(squares).indexOf(square);
 	square.textContent = currentPlayer;
-	board[index] = currentPlayer;
-	if (checkForWin(currentPlayer, board)) {
+	if (checkForWin()) {
 		alert('Player ' + currentPlayer + ' wins!');
 		resetBoard();
 	} else if (checkForDraw()) {
@@ -25,33 +22,22 @@ function handleClick(event) {
 	}
 }
 
-function checkForWin(player, board) {
-  const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  for (let i = 0; i < winningCombos.length; i++) {
-    const [a, b, c] = winningCombos[i];
-    if (board[a] === player && board[b] === player && board[c] === player) {
-      // Display X or O symbol
-      const symbol = player === 'X' ? '❌' : '⭕';
-      document.querySelector('.winner').innerHTML = `${symbol} wins!`;
-
-      // Add crossline across winning squares
-      const winningSquares = document.querySelectorAll(`#${a}, #${b}, #${c}`);
-      winningSquares.forEach(square => square.classList.add('winning-square'));
-
-      return true;
-    }
-  }
-  return false;
+function checkForWin() {
+	const winningCombos = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
+	return winningCombos.some(combo => {
+		return combo.every(index => {
+			return squares[index].textContent === currentPlayer;
+		});
+	});
 }
 
 function checkForDraw() {
@@ -64,10 +50,5 @@ function resetBoard() {
 	squares.forEach(square => {
 		square.textContent = '';
 	});
-	board.fill('');
 	currentPlayer = 'X';
-	document.querySelectorAll('.winning-square').forEach(square => {
-		square.classList.remove('winning-square');
-	});
-	document.querySelector('.winner').innerHTML = '';
 }
